916b2f0bd4ec17edc67aa11f8fe2775abb534fb4
DennyAzevedo <denny_af@yahoo.com.br>
2022-11-19 09:54:59 -0300

    Iniciando o Projeto

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -1,5 +1,23 @@
 import { Request, Response } from 'express';
+import { Product } from '../models/Product';
 
 export const home = (req: Request, res: Response) => {
-	res.render('pages/home');
+	let age: number = 58;
+	let showOld: boolean = false;
+
+	if (age > 50) {
+		showOld = true;
+	}
+	
+	let list = Product.getAll();
+	let expensiveList = Product.getFromPriceAfter(12);
+
+	res.render('pages/home', {
+		firstName: 'Denny',
+		lastName: 'Azevedo',
+		showOld,
+		products: list,
+		expensives: expensiveList,
+		frasesDoDia: []
+	});
 }
